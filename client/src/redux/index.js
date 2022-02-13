a2fc17a852c7a36eb21a9fed1cec0674c23e755f
manzub <jeddachppc@gmail.com>
2022-02-13 20:45:19 +0100

    removed devtools ext

--- client/src/redux/index.js
+++ client/src/redux/index.js
@@ -1,6 +1,6 @@
 import { launchPadInfoReducer, metamaskReducer, tokensAndPairsReducer } from "./reducers";
 import { applyMiddleware, combineReducers, compose, createStore } from '@reduxjs/toolkit'
-import { composeWithDevTools } from "redux-devtools-extension";
+// import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 
 const allReducers = combineReducers({
